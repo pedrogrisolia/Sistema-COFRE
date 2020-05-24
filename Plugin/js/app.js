6e4ba59fe818707bf8b4e7adb36db9ae1659ff12
Pedro Moreira Grisolia <pedromg19@gmail.com>
2020-05-24 01:16:50 -0300

    b

---
+++ Plugin/js/app.js
@@ -0,0 +1,20 @@
+var app = {
+    sendSms: function() {
+        var number = document.getElementById('numberTxt').value.toString(); /* iOS: ensure number is actually a string */
+        var message = document.getElementById('messageTxt').value;
+        console.log("number=" + number + ", message= " + message);
+
+        //CONFIGURATION
+        var options = {
+            replaceLineBreaks: false, // true to replace \n by a new line, false by default
+            android: {
+                intent: 'INTENT'  // send SMS with the native android SMS messaging
+                //intent: '' // send SMS without opening any other app
+            }
+        };
+
+        var success = function () { alert('Message sent successfully'); };
+        var error = function (e) { alert('Message Failed:' + e); };
+        sms.send(number, message, options, success, error);
+    }
+};
